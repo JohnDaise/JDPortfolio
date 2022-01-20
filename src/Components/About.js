150738c0ef0b008a1591c4e6915ac8099059890a
JohnDaiseTruckerTools <jdaise@gmail.com>
2022-01-19 19:15:57 -0500

    basic about page

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,7 +5,13 @@ import '../App.css';
 function About() {
   return (
     <div className="about">
-      ABOUT  
+      <h2>ABOUT</h2>  
+      <p>
+          I am a full stack software engineer with a background as an educator. I have a passion for building
+          simple and intuitive tools that help people solve problems and connect with one another. 
+          My main wheelhouse is working with Javascript and PHP, however I am curious and eager to learn other technologies.
+         <br/>
+      </p>
     </div>
   );
 }
