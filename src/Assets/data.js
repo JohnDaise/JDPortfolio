1ebdf8cb91122f896e1de65e874114741afdb659
JohnDaiseTruckerTools <jdaise@gmail.com>
2022-02-07 21:57:25 -0500

    start of adding projects

---
+++ src/Assets/data.js
@@ -0,0 +1,26 @@
+const projects = [
+    
+    {
+        "title":"Pokemon Search",
+        "deployedUrl":"https://johndaise.github.io/coding-excercise/",
+        "github":"https://github.com/JohnDaise/coding-excercise",
+        "type": "app"
+    },
+    {
+        "title": "Unison",
+        "url": "https://player.vimeo.com/video/350098125?h=90ee0f25c6&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+        "type": "video"
+    },
+    {
+        "title": "SetBuilder",
+        "url": "https://player.vimeo.com/video/354874993?h=880ca16067&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+        "type": "video"
+    },
+    {
+        "title": "Frame",
+        "url": "https://player.vimeo.com/video/348815785?h=411ec3bb52&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+        "type": "video"
+    }
+];
+
+export default projects;
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,22 +1,50 @@
 
 import { Row, Col } from 'antd';
 import '../App.css';
+import projects from './../Assets/data';
 
 
 function Projects() {
   return (
     <div className="projects">
         <h2>PROJECTS</h2>
-        <Row gutter={[16, 16]}>
-            <Col span={12} >Project 1</Col>
-            <Col span={12} >Project 2</Col>
+        <Row gutter={[16, 16]}> 
+              {
+                projects.map((project) =>{
+                
+                  switch(project.type){
+                    case "app":
+                      return (
+                        <Col span={12}>
+                          <h3>{project.title}</h3>
+                          <div>
+                            <image src={project.image} />
+                            <a href={project.deployedUrl} target="_blank">{project.title} Deployed Site</a><br/>
+                            <a href={project.github} target="_blank">GitHub</a>
+                          </div>
+                        </Col>
+                        );
+                      break;
+                    case "video":
+                      return (
+                        <Col span={12}>
+                          <h3>{project.title}</h3>
+                          <div className='vimeo'>
+                            <iframe src={project.url} frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen ></iframe>
+                          </div>
+                        </Col>
+                        );
+                      break;
+                  }
+
+                })
+            }
         </Row>
-        <Row>
-            <Col span={12} >Project 3</Col>
-            <Col span={12} >Project 4</Col>
-        </Row><br/>
+
     </div>
   );
 }
+
+
 
 export default Projects;
